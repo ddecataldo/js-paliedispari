f22814c450013d5ed91ce93bd2a01936ee14f98a
ddecataldo <davidedecataldo92@gmail.com>
2021-10-18 17:42:15 +0200

    Fix

--- palidroma/scripts.js
+++ palidroma/scripts.js
@@ -4,26 +4,26 @@
 * Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// Chiedere all'utente di inserire una parola
-const parolaUtente = prompt("Inserisci una parola");
+function verificaParolaUtente (parolaUtente){
 
-// Scomporre la parola e analizzarla lettara per lettera
-// Il metodo .split() divide l' oggetto String in un Array di strighe separando la stringa in sottostringhe.
-const splitParolaUtente = parolaUtente.split("");
-/* console.log(splitParolaUtente); */
+    // Chiedere all'utente di inserire una parola
+    // const parolaUtente = prompt("Inserisci una parola");
 
-// Invertire l'ordine della parola inserita dall'utente
-// Con il metodo .reverse() possiamo invertire i singoli elementi dell'array. 
-// Il primo elemento sarà occuperà l'ultima posizione del nostro array e l'ultimo elemento occuperà la prima posizione del nostro array.
-const reverseParolaUtente =  splitParolaUtente.reverse();
-/* console.log(reverseParolaUtente); */
+    // Scomporre la parola e analizzarla lettara per lettera
+    // Il metodo .split() divide l' oggetto String in un Array di strighe separando la stringa in sottostringhe.
+    const splitParolaUtente = parolaUtente.split("");
+    /* console.log(splitParolaUtente); */
 
-// Riunire le parole
-// Con il metodo .join() possiamo ricongiunge tutti i singoli caratteri precedentemente separati dal metodo split() e ricomporre la parola.
-const joinParolaUtente = reverseParolaUtente.join("");
-/* console.log(joinParolaUtente); */
+    // Invertire l'ordine della parola inserita dall'utente
+    // Con il metodo .reverse() possiamo invertire i singoli elementi dell'array. 
+    // Il primo elemento sarà occuperà l'ultima posizione del nostro array e l'ultimo elemento occuperà la prima posizione del nostro array.
+    const reverseParolaUtente =  splitParolaUtente.reverse();
+    /* console.log(reverseParolaUtente); */
 
-function verificaParolaUtente (){
+    // Riunire le parole
+    // Con il metodo .join() possiamo ricongiunge tutti i singoli caratteri precedentemente separati dal metodo split() e ricomporre la parola.
+    const joinParolaUtente = reverseParolaUtente.join("");
+    /* console.log(joinParolaUtente); */
 
     if(parolaUtente === joinParolaUtente){
         console.log("La parola " + parolaUtente + " è palindroma.")
@@ -32,7 +32,7 @@ function verificaParolaUtente (){
     }
 
 }
-
-console.log(verificaParolaUtente());
+const utente = prompt("Inserisci una parola");
+verificaParolaUtente(utente);
 
 
